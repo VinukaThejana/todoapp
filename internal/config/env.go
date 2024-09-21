64bc7780c6a7fe9f42854869219e3e8fa1c48aff
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-09-21 18:21:20 +0530

    feat(env): added access token details

--- internal/config/env.go
+++ internal/config/env.go
@@ -15,11 +15,15 @@ type Env struct {
 	DatabaseURL            string        `mapstructure:"DATABASE_URL" validate:"required"`
 	RedisURL               string        `mapstructure:"REDIS_URL" validate:"required"`
 	RedisPassword          string        `mapstructure:"REDIS_PASSWORD" validate:"required"`
-	RefreshTokenExpiresIn  time.Duration `mapstructure:"REFRESH_TOKEN_EXPIRES_IN" validate:"required"`
 	RefreshTokenMaxAge     string        `mapstructure:"REFRESH_TOKEN_MAX_AGE" validate:"required"`
 	RefreshTokenPrivateKey string        `mapstructure:"REFRESH_TOKEN_PRIVATE_KEY" validate:"required"`
 	RefreshTokenPublicKey  string        `mapstructure:"REFRESH_TOKEN_PUBLIC_KEY" validate:"required"`
+	AccessTokenMaxAge      string        `mapstructure:"ACCESS_TOKEN_MAX_AGE" validate:"required"`
+	AccessTokenPrivateKey  string        `mapstructure:"ACCESS_TOKEN_PRIVATE_KEY" validate:"required"`
+	AccessTokenPublicKey   string        `mapstructure:"ACCESS_TOKEN_PUBLIC_KEY" validate:"required"`
 	Environ                string        `mapstructure:"ENVIRON" validate:"required,oneof=dev stg prod"`
+	AccessTokenExpiresIn   time.Duration `mapstructure:"ACCESS_TOKEN_EXPIRES_IN" validate:"required"`
+	RefreshTokenExpiresIn  time.Duration `mapstructure:"REFRESH_TOKEN_EXPIRES_IN" validate:"required"`
 }
 
 func (e *Env) Load(path ...string) {
