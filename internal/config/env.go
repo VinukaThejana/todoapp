28f67d8d160667cbce62c0423f30500c7a140e8b
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-09-21 11:23:26 +0530

    feat(env): added redis

--- internal/config/env.go
+++ internal/config/env.go
@@ -9,6 +9,8 @@ type Env struct {
 	TodogRPCPort   string `mapstructure:"TODO_GRPC_PORT" validate:"required"`
 	APIGatewayPort string `mapstructure:"API_GATEWAY_PORT" validate:"required"`
 	DatabaseURL    string `mapstructure:"DATABASE_URL" validate:"required"`
+	RedisURL       string `mapstructure:"REDIS_URL" validate:"required"`
+	RedisPassword  string `mapstructure:"REDIS_PASSWORD" validate:"required"`
 	Environ        string `mapstructure:"ENVIRON" validate:"required,oneof=dev stg prod"`
 }
 
