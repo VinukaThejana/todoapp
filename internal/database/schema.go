7146fc08e9c62952bfd3ce4077aaea58d5d2629c
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-09-21 10:39:13 +0530

    feat(schema): added the users, todo and session schemas

---
+++ internal/database/schema.go
@@ -0,0 +1,56 @@
+// Package database provides the models for the database tables
+package database
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+// Tables contains the list of tables to be created
+var Tables = []struct {
+	Name   string
+	Schema any
+}{
+	{
+		Name:   "users",
+		Schema: User{},
+	},
+	{
+		Name:   "todos",
+		Schema: Todo{},
+	},
+	{
+		Name:   "sessions",
+		Schema: Session{},
+	},
+}
+
+// User is a model for the user table
+type User struct {
+	gorm.Model
+	Name     string `gorm:"not null"`
+	Email    string `gorm:"type:varchar(255);unique_index"`
+	Username string `gorm:"type:varchar(100);unique_index"`
+	Password string `gorm:"not null"`
+}
+
+// Todo is a model for the todo table
+type Todo struct {
+	gorm.Model
+	Title       string `gorm:"not null"`
+	Description string `gorm:"not null"`
+	Content     string `gorm:"not null"`
+	Completed   bool   `gorm:"not null"`
+	UserID      uint   `gorm:"not null"`
+	User        User   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
+
+// Session is a model for the session table
+type Session struct {
+	ID        uint `gorm:"primarykey"`
+	LoginAt   time.Time
+	ExpiresAt int64 `gorm:"not null"`
+	UserID    uint  `gorm:"not null"`
+	User      User  `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
