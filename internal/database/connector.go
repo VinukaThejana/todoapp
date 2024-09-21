85571d6b544cf4650013957cfb1c077921868293
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-09-21 10:39:33 +0530

    feat(datbase): created the database connector

---
+++ internal/database/connector.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/VinukaThejana/go-utils/logger"
+	env "github.com/VinukaThejana/todoapp/internal/config"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+// Init initializes the database connection
+func Init(e *env.Env) *gorm.DB {
+	db, err := gorm.Open(sqlite.Open(e.DatabaseURL), &gorm.Config{})
+	if err != nil {
+		logger.Errorf(fmt.Errorf("Failed to connect to database: %v", err))
+	}
+
+	return db
+}
