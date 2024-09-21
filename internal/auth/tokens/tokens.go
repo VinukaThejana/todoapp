890c43727fc59ed0119d61b27e720da7ac1c33ae
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-09-21 18:27:42 +0530

    feat(tokens): added the token details

--- internal/auth/tokens/tokens.go
+++ internal/auth/tokens/tokens.go
@@ -1,2 +1,10 @@
 // Package tokens creates and validates JWT tokens
 package tokens
+
+type tokendetails struct {
+	Token     string
+	JTI       string
+	ExpiresIn int64
+	Iat       int64
+	Sub       uint
+}
