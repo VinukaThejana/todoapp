5bc90bd1f8bcf974be8be778d80261d75a4bad41
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-09-22 09:59:07 +0530

    feat(middleware): added the ability to check the access token

--- internal/api/middleware/middleware.go
+++ internal/api/middleware/middleware.go
@@ -2,11 +2,16 @@
 package middleware
 
 import (
+	"context"
 	"net/http"
 	"strings"
 
 	"github.com/bytedance/sonic"
 	"github.com/rs/zerolog/log"
+)
+
+const (
+	RefreshToken = "todoapp_refresh_token"
 )
 
 // ContentJSON is a middleware that checks if the content type is application/json
@@ -29,5 +34,35 @@ func ContentJSON(next http.Handler) http.Handler {
 		}
 
 		next.ServeHTTP(w, r)
+	})
+}
+
+// RefreshTokenPresent is a middleware that checks if the refresh token is present in the request
+func RefreshTokenPresent(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		refreshTokenC, err := r.Cookie("todoapp_refresh_token")
+		if err != nil {
+			log.Error().Msg("refresh token not present")
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), RefreshToken, refreshTokenC.Value)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+// AuthTokenPresent is a middleware that checks if the auth token is present in the request
+func AuthTokenPresent(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authToken := r.Header.Get("Authorization")
+		if authToken == "" {
+			log.Error().Msg("auth token not present")
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "todoapp_access_token", authToken)
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
