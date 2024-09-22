32e2233218f162b3bce71dad63033dea23e51489
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-09-22 20:29:21 +0530

    feat(lib): added the ability to validate the username or the email depending on what is given

---
+++ internal/lib/validate.go
@@ -0,0 +1,30 @@
+package lib
+
+import (
+	"net/mail"
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
+)
+
+// ValiateEmailOrUsername is a custom validator for validating email or username
+func ValiateEmailOrUsername(fl validator.FieldLevel) bool {
+	username := fl.Parent().FieldByName("Username").String()
+	email := fl.Parent().FieldByName("Email").String()
+
+	if username == "" && email == "" {
+		return false
+	}
+
+	if email != "" {
+		_, err := mail.ParseAddress(email)
+		return err == nil
+	}
+
+	regex, err := regexp.Compile(`^[a-zA-Z0-9_.#]{1,20}$`)
+	if err != nil {
+		return false
+	}
+
+	return regex.MatchString(username)
+}
