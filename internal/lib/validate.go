cb5c49be89e259fdbedd613a20fa8cbee6057665
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-09-22 20:32:57 +0530

    feat(lib): added the ability to validate the username

--- internal/lib/validate.go
+++ internal/lib/validate.go
@@ -7,6 +7,21 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
+// ValidateUsername is a custom validator for validating username
+func ValidateUsername(fl validator.FieldLevel) bool {
+	username := fl.Parent().FieldByName("Username").String()
+	if username == "" {
+		return false
+	}
+
+	regex, err := regexp.Compile(`^[a-zA-Z0-9_.#]{1,20}$`)
+	if err != nil {
+		return false
+	}
+
+	return regex.MatchString(username)
+}
+
 // ValiateEmailOrUsername is a custom validator for validating email or username
 func ValiateEmailOrUsername(fl validator.FieldLevel) bool {
 	username := fl.Parent().FieldByName("Username").String()
