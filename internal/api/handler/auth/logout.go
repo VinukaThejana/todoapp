7df33106b80aa7ea9d4f3d0798d4e3950ce1d0ac
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-10-01 17:31:48 +0530

    feat(auth): remove the cookies from the logout

--- internal/api/handler/auth/logout.go
+++ internal/api/handler/auth/logout.go
@@ -2,11 +2,13 @@ package auth
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/VinukaThejana/todoapp/internal/api/grpc"
 	"github.com/VinukaThejana/todoapp/internal/api/handler"
 	"github.com/VinukaThejana/todoapp/internal/api/middleware"
 	env "github.com/VinukaThejana/todoapp/internal/config"
+	"github.com/VinukaThejana/todoapp/internal/enums"
 	"github.com/VinukaThejana/todoapp/pkg/auth"
 	"github.com/redis/go-redis/v9"
 	"github.com/rs/zerolog/log"
@@ -38,13 +40,32 @@ func Logout(
 
 		switch st.Code() {
 		case codes.Unauthenticated:
-			handler.JSONr(w, http.StatusUnauthorized, "Invalid password")
+			handler.JSONr(w, http.StatusUnauthorized, "Not authorized to perform this action")
 			return
 		default:
 			handler.JSONr(w, http.StatusInternalServerError, "Internal server error")
 			return
 		}
 	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "todoapp_refresh_token",
+		Value:    "",
+		Path:     "/",
+		HttpOnly: true,
+		Expires:  time.Now().UTC().Add(-24 * time.Hour),
+		Secure:   e.Environ == string(enums.Prd),
+		Domain:   e.Domain,
+	})
+	http.SetCookie(w, &http.Cookie{
+		Name:     "todoapp_session_token",
+		Value:    "",
+		Path:     "/",
+		HttpOnly: false,
+		Expires:  time.Now().UTC().Add(-24 * time.Hour),
+		Secure:   e.Environ == string(enums.Prd),
+		Domain:   e.Domain,
+	})
 
 	handler.JSONr(w, http.StatusOK, "Successfully logged out")
 }
